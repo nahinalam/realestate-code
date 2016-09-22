83f409c305e6f9e2913ab15f90ebffca5d569483
nahinalam <nahinalam@hotmail.com>
2016-09-22 18:05:39 -0400

    took off lib

--- RealEstate/src/test.java
+++ RealEstate/src/test.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class test
  */
-@WebServlet("/test")
+//@WebServlet("/test")
 public class test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
